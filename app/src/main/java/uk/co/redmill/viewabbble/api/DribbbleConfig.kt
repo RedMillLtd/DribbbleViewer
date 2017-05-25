220e7c32e9a7f93ab8742c3cb5ecd9691624bd1b
David Miller <dave@Davids-MacBook-Pro-2.local>
2017-05-25 17:10:19 +0100

    Removed unused field

--- app/src/main/java/uk/co/redmill/viewabbble/api/DribbbleConfig.kt
+++ app/src/main/java/uk/co/redmill/viewabbble/api/DribbbleConfig.kt
@@ -6,4 +6,4 @@ import android.content.Context
  * Created by dave on 5/18/17.
  */
 
-class DribbbleConfig(cont: Context, val authUrl: String, var baseUrl: String?)
+class DribbbleConfig(val authUrl: String, var baseUrl: String?)
